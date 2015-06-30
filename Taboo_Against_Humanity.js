d0b106d4fc6964ed6f226d13a49df4558c9bbd5e
mikewchan <mike@mikewchan.com>
2015-06-30 10:40:25 -0400

    first commit

---
+++ Taboo_Against_Humanity.js
@@ -0,0 +1,28 @@
+/* TabooCards is the global variable, cards is the name of the collection */
+
+TabooCards = new Mongo.Collection('cards');
+
+console.log("Hello world");
+
+/* the if statement 'isClient' is there to tell the code to only run on the client */
+
+
+if(Meteor.isClient){
+
+/*  Template keyword searches through our code's templates, 
+'cardItem' refers to the 'cardItem' template in the HTML file, 'card' is the actual 
+template helper function. */
+  
+  Template.cardItem.helpers ({
+    card: function(){
+      return TabooCards.find()
+  },
+    otherHelperFunction: function(){
+      return "Some other function"
+    }
+});
+}
+
+if(Meteor.isServer){
+  
+}
